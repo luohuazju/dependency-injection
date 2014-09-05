a6f17c8f8766d5fb924b9808806ff02915999a67
Sarah Ryan <sarah@theartoflogic.com>
2014-09-05 13:07:13 -0700

    Added additional methods and other various updates
    - Added `hasParameter()` method to check whether a parameter exists.
    - Added `has()` method to check whether a service exists.
    - Made the `params` private so they're only accessible using the gettings and setters.

--- test/container.js
+++ test/container.js
@@ -32,6 +32,8 @@ suite('Dependency Injection', function() {
 
         // Do assertions
         assert.equal(container.getParameter('test.value'), 'ok');
+        assert.equal(container.getParameter('invalid'), undefined);
+        assert.equal(container.getParameter('invalid.invalid'), undefined);
 
     });
 
@@ -54,13 +56,73 @@ suite('Dependency Injection', function() {
         container.mergeParameters({ test2: { deep: { value: 'new value3' } } });
 
         // Do assertions
-        assert.equal(container.parameters.test1, 'value1');
-        assert.equal(container.parameters.test2.value, 'value2');
-        assert.equal(container.parameters.test2.deep.value, 'new value3');
+        assert.equal(container.getParameter('test1'), params.test1);
+        assert.equal(container.getParameter('test2'), params.test2);
+        assert.equal(container.getParameter('test2.value'), params.test2.value);
+        assert.equal(container.getParameter('test2.deep'), params.test2.deep);
+        assert.equal(container.getParameter('test2.deep.value'), params.test2.deep.value);
+        assert.equal(container.getParameter('test2.deep.invalid'), undefined);
 
     });
 
-    test('set single service', function() {
+    test('has parameter', function() {
+
+        var params = {
+            test1: 'value1',
+            test2: {
+                value: 'value2',
+                deep: {
+                    value: 'value3'
+                }
+            },
+            test3: [
+                'value4',
+                'value5',
+                {
+                    value: 'value6',
+                    deep: {
+                        value: 'value7'
+                    }
+                }
+            ]
+        };
+
+        // Instantiate the service container
+        var container = new Container(params);
+
+        // Do positive assertions
+        assert.ok(container.hasParameter('test1'));
+        assert.ok(container.hasParameter('test2'));
+        assert.ok(container.hasParameter('test2.value'));
+        assert.ok(container.hasParameter('test2.deep'));
+        assert.ok(container.hasParameter('test2.deep.value'));
+        assert.ok(container.hasParameter('test3'));
+        assert.ok(container.hasParameter('test3.0'));
+        assert.ok(container.hasParameter('test3.1'));
+        assert.ok(container.hasParameter('test3.2'));
+        assert.ok(container.hasParameter('test3.2.value'));
+        assert.ok(container.hasParameter('test3.2.deep'));
+        assert.ok(container.hasParameter('test3.2.deep.value'));
+        assert.ok(container.hasParameter('test1.invalid') == false);
+        assert.ok(container.hasParameter('test2.deep.invalid') == false);
+        assert.ok(container.hasParameter('invalid') == false);
+        assert.ok(container.hasParameter('invalid.invalid') == false);
+        assert.ok(container.hasParameter('invalid.invalid.invalid') == false);
+
+    });
+
+    test('has service', function() {
+
+        // Instantiate the service container
+        var container = new Container();
+
+        // Do assertions
+        assert.ok(container.has('service-container'));
+        assert.ok(container.has('invalid') == false);
+
+    });
+
+    test('set service', function() {
 
         var params = {
             test_service: {
@@ -75,7 +137,7 @@ suite('Dependency Injection', function() {
         var container = new Container(params);
 
         // Set the service
-        container.set('test_service', {
+        container.set('test', {
             path: '%test_service.path%',
             arguments: [
                 '%test_service.arg1%'
@@ -85,21 +147,25 @@ suite('Dependency Injection', function() {
             ],
             set: {
                 arg3: '%test_service.arg3%'
-            }
+            },
+            isFactory: true
         });
 
+        assert.ok(container.has('test'));
+
         // Get the service
-        var service = container.get('test_service');
+        var service = container.get('test');
 
         // Do assertions
-        assert.equal(typeof(service), 'function');
+        assert.ok(service instanceof Object);
+        assert.ok(service.set instanceof Function);
         assert.equal(service.arg1, params.test_service.arg1);
         assert.equal(service.arg2, params.test_service.arg2);
         assert.equal(service.arg3, params.test_service.arg3);
 
     });
 
-    test('singleton service', function() {
+    test('get singleton service', function() {
 
         var params = {
             test_service: {
@@ -114,28 +180,30 @@ suite('Dependency Injection', function() {
         var container = new Container(params);
 
         // Set the service
-        container.set('test_service', {
+        container.set('test', {
             path: '%test_service.path%',
             arguments: [
                 '%test_service.arg1%'
             ]
         });
 
+        assert.ok(container.has('test'));
+
         // Get the service
-        var service = container.get('test_service');
+        var service = container.get('test');
 
         // Override an argument
         service.arg1 = 'new value1';
 
         // Get the service again
-        var service = container.get('test_service');
+        var service = container.get('test');
 
         // Do assertions
         assert.equal(service.arg1, 'new value1');
 
     });
 
-    test('non-singleton service', function() {
+    test('get non-singleton service', function() {
 
         var params = {
             test_service: {
@@ -147,7 +215,7 @@ suite('Dependency Injection', function() {
         var container = new Container(params);
 
         // Set the service
-        container.set('test_service', {
+        container.set('test', {
             singleton: false,
             path: '%test_service.path%',
             arguments: [
@@ -155,14 +223,16 @@ suite('Dependency Injection', function() {
             ]
         });
 
+        assert.ok(container.has('test'));
+
         // Get the service
-        var service = container.get('test_service');
+        var service = container.get('test');
 
         // Override an argument
         service.arg1 = 'new value';
 
         // Get the service again
-        var service = container.get('test_service');
+        var service = container.get('test');
 
         // Do assertions
         assert.equal(service.arg1, 'value');
